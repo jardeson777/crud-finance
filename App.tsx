71da43866e0e280853db1a3845ffb9529f634827
jnogueira <jnogueira@marlin.com.br>
2022-02-07 23:19:00 -0300

    feat: eslint, prittier, theme e stitches configurados

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { MyTabs } from './src/routes';
 
 export default function App() {
-  return (
-    <MyTabs />
-  );
+  return <MyTabs />;
 }
-
-
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createStitches } from 'stitches-native';
+
+export const { styled } = createStitches({
+  theme: {
+    colors: {
+      white: '#fff',
+      purple1: '#cbccff',
+      purple2: '#b2b4f0',
+      purple3: '#999de1',
+      purple4: '#7f85d2',
+      purple5: '#666dc3'
+    }
+  }
+});
